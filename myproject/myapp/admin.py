cc53f3cd7dcf86e4012034f9eb640ae3e511c8a7
ANDE JYOTHI VENKATA PHANINDRA <phanindraande3@gmail.com>
2025-09-30 15:15:12 +0530

    Delete myproject directory

--- myproject/myapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Review
-
-@admin.register(Review)
-class ReviewAdmin(admin.ModelAdmin):
-    # Correctly uses 'submission_date'
-    list_display = ('user', 'rating', 'review_text', 'submission_date') 
-    list_filter = ('rating', 'submission_date') 
-    search_fields = ['user__username', 'review_text']
